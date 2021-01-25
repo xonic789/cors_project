343454ff6ad2fee451efbc8ba4de146958b32c29
Yoon <cksal5911@kakao.com>
2021-01-25 22:33:53 +0900

    Global styles 정의

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AppLayout from './components/AppLayout';
 import Login from './features/login/Login';
+import GlobalStyle from './styles/GlobalStyles';
 
 function App():JSX.Element {
   return (
     <div className="App">
+      <GlobalStyle />
       <div>
         <BrowserRouter>
           <Switch>
             <Route path="/" exact component={Login} />
+            <Route path="/home" exact component={AppLayout} />
           </Switch>
         </BrowserRouter>
-        <a href="/oauth2/authorization/google">sample button</a>
       </div>
     </div>
   );
