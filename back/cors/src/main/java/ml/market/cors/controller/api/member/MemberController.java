767940db49b54a888d7c9d804d0658076b44c3e1
ehaakdl <ehaakdl@naver.com>
2021-01-31 20:51:12 +0900

    정기 커밋

--- back/cors/src/main/java/ml/market/cors/controller/api/member/MemberController.java
+++ back/cors/src/main/java/ml/market/cors/controller/api/member/MemberController.java
@@ -9,6 +9,7 @@ import ml.market.cors.domain.util.ResponseEntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
