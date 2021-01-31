25c2070fa074f0974996d0e5950ead2049921bc6
Yoon <cksal5911@kakao.com>
2021-01-31 19:35:53 +0900

    Categoty ui 1차
    카테고리 json으로  ui그림

---
+++ front/src/features/postList/CategoryMenu.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import CategoryJSON from './Category.json';
+
+interface CategoryMenuPropsInterFace {
+  onMenuClose: (e : React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
+  isOppend: boolean,
+}
+const BurgerMenuWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100vw;
+  min-height: 100vh;
+  background-color: white;
+  overflow: auto;
+`;
+const BurgerMenuHeader = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: white;
+  border-bottom: 1px solid #e8e8e8;
+  padding: 10px;
+`;
+const BurgerMenuBody = styled.div`
+  display:flex;
+  overflow: scroll;
+`;
+const BurgerMenuList = styled.ul`
+  margin-top: 80px;
+  margin-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  min-height: 100vh;
+  position: sticky;
+`;
+const BurgerMenuListTwo = styled.ul`
+  margin-top: 80px;
+  margin-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  min-height: 100vh;
+`;
+const BurgerMenuItem = styled.li`
+  padding: 10px 30px;
+  font-size: 20px;
+`;
+const CloseButtton = styled.button`
+  background: white;
+  border: 0;
+  & img {
+    width: 25px;
+  }
+`;
+const CategoryMenu = ({ onMenuClose, isOppend }:CategoryMenuPropsInterFace):JSX.Element => {
+  const [contentIndex, setContentIndex] = useState<number>(0);
+  const onChangeCategotyTab = (index: number) => {
+    setContentIndex(index);
+  };
+  return (
+    <BurgerMenuWrapper>
+      <BurgerMenuHeader>
+        <CloseButtton type="button" onClick={onMenuClose}>
+          <img src="/images/icons/x.png" alt="x_button" />
+        </CloseButtton>
+      </BurgerMenuHeader>
+      <BurgerMenuBody>
+        <BurgerMenuList>
+          {CategoryJSON.Category.map(
+            (c, i) => (
+              <BurgerMenuItem onClick={() => onChangeCategotyTab(i)}>
+                {c.title}
+              </BurgerMenuItem>
+            ),
+          )}
+        </BurgerMenuList>
+        <BurgerMenuListTwo>
+          <BurgerMenuItem>전체보기</BurgerMenuItem>
+          {CategoryJSON.Category[contentIndex].depth.map((c) => (
+            <BurgerMenuItem>
+              {c.title}
+            </BurgerMenuItem>
+          ))}
+        </BurgerMenuListTwo>
+      </BurgerMenuBody>
+    </BurgerMenuWrapper>
+  );
+};
+
+export default CategoryMenu;
