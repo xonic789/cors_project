2672e54c3344fdec6bc9eda15cd67db92eb0ed6b
Yoon <cksal5911@kakao.com>
2021-02-08 20:43:46 +0900

    [찬미]Util 함수 정의

--- front/src/utils/categoryFormatUtil.ts
+++ front/src/utils/categoryFormatUtil.ts
@@ -1,7 +1,7 @@
 interface categoryInterface {
   cid: number, oneDepth: string, twoDepth: string, threeDepth: string, fourDepth: string, fiveDepth: string,
 }
-const CategoryFormat = (category: categoryInterface) => {
+const CategoryFormatUtil = (category: categoryInterface) => {
   const cate = [category.oneDepth, category.twoDepth, category.threeDepth, category.fourDepth, category.fiveDepth];
   const newCate = [];
   for (let i = 0; i < cate.length; i++) {
@@ -14,4 +14,4 @@ const CategoryFormat = (category: categoryInterface) => {
   return newCate.join('>');
 };
 
-export default CategoryFormat;
+export default CategoryFormatUtil;
