485b921258e1d4c7373afa26a24fe23eed762e25
hyeongkyuPark <phg0644@gmail.com>
2021-02-15 09:05:07 +0900

    merge complete

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -4,38 +4,26 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
 import reportWebVitals from './reportWebVitals';
 import rootSaga from './rootSaga';
 import rootReducer from './rootReducers';
 import App from './App';
 import './index.css';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [...getDefaultMiddleware(), logger, sagaMiddleware];
 
 const store = configureStore({
-  reducer: persistReducer(persistConfig, rootReducer),
+  reducer: rootReducer,
   middleware,
 });
 sagaMiddleware.run(rootSaga);
 
-const persistor = persistStore(store);
-
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </React.StrictMode>
   </Provider>,
   document.getElementById('root'),
