907b1f0bbe03c802a04ec05aa390f1a6f063015f
phg0644 <phg0644@gmail.com>
2021-02-20 12:58:06 +0900

    keep

--- front/src/pages/myPage/myMarket/MyMarket.tsx
+++ front/src/pages/myPage/myMarket/MyMarket.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link, useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMyMarketListRequest } from './myMarketSlice';
+import numberArrayUtill from '../../../utils/numberArrayUtill';
 
 const Layout = styled.div`
   display: flex;
@@ -65,7 +68,73 @@ const Footer = styled.div`
   }
 `;
 
+const PaginationBox = styled.div`
+  display: flex;
+  padding: 1.5em 0;
+  & img {
+    cursor: pointer;
+    width: 1em;
+    height: 1em;
+  }
+`;
+
+const PrevPage = styled.img``;
+
+const PageNumbers = styled.div`
+  display: flex;
+`;
+
+const PageNumber = styled.div`
+`;
+
+const PageLink = styled(Link)`
+  padding: 0 0.5em;
+`;
+
+const NextPage = styled.img`
+  width: 1em;
+  height: 1em;
+  transform: rotate(180deg);
+`;
+
 function MyMarket():JSX.Element {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [page, setPage] = useState<number>(0);
+  const { mySaleArticle, totalPage, isGetMyPurchaseArticlesLoading } = useSelector((state: any) => state.myPageSlice.mySaleArticleSlice);
+  const progressForm = (progress: 'COMPLETED' | 'HIDE' | 'TRADING' | 'POSTING'): { text: string, background: string } => {
+    const resultProgress = {
+      COMPLETED: { text: '거래완료', background: '#1e1e1e' },
+      HIDE: { text: '숨김', background: '#7e7e7e' },
+      TRADING: { text: '예약중', background: '#62ff7d' },
+      POSTING: { text: '거래중', background: '#265290' },
+    };
+
+    return resultProgress[progress];
+  };
+
+  const onClickPrevPage = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    const prevPage = page - 1;
+
+    if (prevPage < 0) {
+      e.preventDefault();
+    } else {
+      setPage(0);
+    }
+  };
+  const onClickNextPage = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    const nextPage = page + 1;
+
+    if (nextPage > totalPage - 1) {
+      e.preventDefault();
+    } else {
+      setPage(totalPage - 1);
+    }
+  };
+
+  useEffect(() => {
+    dispatch(getMyMarketListRequest(page));
+  }, [dispatch, page]);
   return (
     <Layout>
       <Header>
@@ -78,7 +147,31 @@ function MyMarket():JSX.Element {
         콘텐트
       </Content>
       <Footer>
-        푸터
+        <PaginationBox>
+          <PrevPage onClick={onClickPrevPage} src="/images/icons/back.png" />
+          <PageNumbers>
+            {
+              !totalPage
+                ? (
+                  <PageNumber>
+                    <PageLink to="/mypage/markets">1</PageLink>
+                  </PageNumber>
+                )
+                : (
+                  <>
+                    {
+                      numberArrayUtill(totalPage).map((i) => (
+                        <PageNumber>
+                          <PageLink onClick={() => setPage(i - 1)} to={`/mypage/markets?page=${i - 1}`}>{i}</PageLink>
+                        </PageNumber>
+                      ))
+                    }
+                  </>
+                )
+            }
+          </PageNumbers>
+          <NextPage onClick={onClickNextPage} src="/images/icons/back.png" />
+        </PaginationBox>
       </Footer>
     </Layout>
   );
