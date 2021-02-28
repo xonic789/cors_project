037bf930fa09c591b1b601228a5d857b23687e10
SeungWooL <xonic789@naver.com>
2021-02-28 18:35:04 +0900

    리드미 커밋

--- back/cors/src/main/java/ml/market/cors/config/chat/WebSockConfig.java
+++ back/cors/src/main/java/ml/market/cors/config/chat/WebSockConfig.java
@@ -27,7 +27,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/ws").withSockJS();
+        registry.addEndpoint("/api/ws").withSockJS();//403 cors?
     }
 
     @Override
